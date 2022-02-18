a017020d908b90387491499eaf55a878c2d5de27
manu-grande-lope <manugrande@gmail.com>
2022-02-18 18:08:12 +0100

    feat: fetch and observer implemented, not tested yet

--- reddit-vs-infinite-scroll/src/pages/redit-list-container.jsx
+++ reddit-vs-infinite-scroll/src/pages/redit-list-container.jsx
@@ -1,8 +1,48 @@
+import { useRef, useState, useEffect } from "react"
 
+export default function ReditListContainer() {
 
-export default function ReditListContainer(){
+    const [after, updateAfter] = useState('')
+    const [items, setItems] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
 
-    return(
+    const lastRef = useRef()
+
+    useEffect(() => {
+        setIsLoading(true)
+
+        fetch(`https://www.reddit.com/r/coding.json?after=${after}`)
+            .then(r => r.jason())
+            .then(d => {
+                updateAfter(d.data.after)
+                setItems(previousvalue => previousvalue.concat(d.data.children))
+                setIsLoading(false)
+            })
+    }, [])
+
+    useEffect(() => {
+        if (lastRef.current !== undefined) {
+            const observer = new IntersectionObserver((posts) => {
+                if (posts[0].isIntersecting && !isLoading) {
+                    setIsLoading(true)
+
+                    fetch(`https://www.reddit.com/r/coding.json?after=${after}`)
+                        .then(r => r.json())
+                        .then(d => {
+                            updateAfter(d.data.after)
+                            setItems(previousvalue => previousvalue.concat(d.data.children))
+                            setIsLoading(false)
+                        })
+                }
+            }, {
+                rootMargin: '150px'
+            })
+            observer.observe(lastRef.current)
+            return ()=>observer.disconnect()
+        }
+    })
+
+    return (
         <div className="list-page">
             <div className="list-container">
 
